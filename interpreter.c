1f929b4a337133b7962c20a724641429a723cd80
Max Bernstein <emacs@fb.com>
2021-01-13 00:01:15 -0800

    Reorganize code
    Functions and data types should be defined as close to their first use
    as possible.
    
    Also split `eval_code` into `eval_code_uncached` and `eval_code_cached`.

--- interpreter.c
+++ interpreter.c
@@ -7,19 +7,6 @@
 // appeal.
 
 typedef enum {
-  // Load a constant from the constant array at index `arg'.
-  CONST,
-  // Load a value from the arguments array at index `arg'.
-  ARG,
-  // Add stack[-2] + stack[-1].
-  ADD,
-  // Pop the top of the stack and print it.
-  PRINT,
-  // Halt the machine.
-  HALT,
-} Bytecode;
-
-typedef enum {
   kInt,
   kStr,
 } ObjectType;
@@ -39,30 +26,6 @@ Object new_int(int value) {
 Object new_str(const char *value) {
   return (Object){.type = kStr, .str_value = value};
 }
-
-typedef enum {
-  kAdd,
-  kPrint,
-
-  kUnknownSymbol = kPrint + 1,
-} Symbol;
-
-// Note: this takes advantage of the fact that in C, not putting anything
-// between the parentheses means that this function can take any number of
-// arguments.
-typedef Object (*Method)();
-
-typedef struct {
-  Symbol name;
-  Method method;
-} MethodDefinition;
-
-typedef unsigned char byte;
-
-typedef struct {
-  ObjectType key;
-  Method value;
-} CachedValue;
 
 #define CHECK(cond) \
   do {              \
@@ -99,6 +62,23 @@ Object str_print(Object obj) {
   return obj;
 }
 
+typedef enum {
+  kAdd,
+  kPrint,
+
+  kUnknownSymbol = kPrint + 1,
+} Symbol;
+
+// Note: this takes advantage of the fact that in C, not putting anything
+// between the parentheses means that this function can take any number of
+// arguments.
+typedef Object (*Method)();
+
+typedef struct {
+  Symbol name;
+  Method method;
+} MethodDefinition;
+
 static const MethodDefinition kIntMethods[] = {
     {kAdd, int_add},
     {kPrint, int_print},
@@ -116,6 +96,32 @@ static const MethodDefinition *kTypes[] = {
     [kStr] = kStrMethods,
 };
 
+typedef unsigned char byte;
+
+typedef struct {
+  ObjectType key;
+  Method value;
+} CachedValue;
+
+typedef struct {
+  // Array of `num_opcodes' (op, arg) pairs (total size `num_opcodes' * 2).
+  byte *bytecode;
+  int num_opcodes;
+  // Array of `num_opcodes' elements.
+  CachedValue *caches;
+} Code;
+
+typedef enum {
+  // Load a value from the arguments array at index `arg'.
+  ARG,
+  // Add stack[-2] + stack[-1].
+  ADD,
+  // Pop the top of the stack and print it.
+  PRINT,
+  // Halt the machine.
+  HALT,
+} Opcode;
+
 #define ARRAYSIZE(ARR) (sizeof(ARR) / sizeof(ARR)[0])
 
 Method lookup_method(ObjectType type, Symbol name) {
@@ -129,31 +135,48 @@ Method lookup_method(ObjectType type, Symbol name) {
   CHECK(false && "could not find method");
 }
 
-typedef struct {
-  // Array of `num_opcodes' (op, arg) pairs (total size `num_opcodes' * 2).
-  byte *bytecode;
-  int num_opcodes;
-  // Array of `num_consts' constant values.
-  Object *consts;
-  int num_consts;
-  // Array of `num_opcodes' elements.
-  CachedValue *caches;
-} Code;
-
-Code new_code(byte *bytecode, int num_opcodes, Object *consts,
-              int num_consts) {
-  Code result;
-  result.bytecode = bytecode;
-  result.num_opcodes = num_opcodes;
-  result.consts = consts;
-  result.num_consts = num_consts;
-  result.caches = calloc(num_opcodes, sizeof *result.caches);
-  return result;
-}
-
 static unsigned kBytecodeSize = 2;
 
-void eval_code(Code *code, Object *args, int nargs) {
+void eval_code_uncached(Code *code, Object *args, int nargs) {
+  int pc = 0;
+#define STACK_SIZE 100
+  Object stack_array[STACK_SIZE];
+  Object *stack = stack_array;
+#define PUSH(x) *stack++ = (x)
+#define POP() *--stack
+  while (true) {
+    Opcode op = code->bytecode[pc];
+    byte arg = code->bytecode[pc + 1];
+    switch (op) {
+      case ARG:
+        CHECK(arg < nargs && "out of bounds arg");
+        PUSH(args[arg]);
+        break;
+      case ADD: {
+        Object right = POP();
+        Object left = POP();
+        Method method = lookup_method(left.type, kAdd);
+        Object result = (*method)(left, right);
+        PUSH(result);
+        break;
+      }
+      case PRINT: {
+        Object obj = POP();
+        Method method = lookup_method(obj.type, kPrint);
+        (*method)(obj);
+        break;
+      }
+      case HALT:
+        return;
+      default:
+        fprintf(stderr, "unknown opcode %d\n", op);
+        abort();
+    }
+    pc += kBytecodeSize;
+  }
+}
+
+void eval_code_cached(Code *code, Object *args, int nargs) {
   int pc = 0;
 #define STACK_SIZE 100
   Object stack_array[STACK_SIZE];
@@ -162,13 +185,9 @@ void eval_code(Code *code, Object *args, int nargs) {
 #define POP() *--stack
 #define CACHE_AT(pc) code->caches[(pc) / kBytecodeSize]
   while (true) {
-    byte op = code->bytecode[pc];
+    Opcode op = code->bytecode[pc];
     byte arg = code->bytecode[pc + 1];
     switch (op) {
-      case CONST:
-        CHECK(arg < code->num_consts && "out of bounds const");
-        PUSH(code->consts[arg]);
-        break;
       case ARG:
         CHECK(arg < nargs && "out of bounds arg");
         PUSH(args[arg]);
@@ -205,18 +224,20 @@ void eval_code(Code *code, Object *args, int nargs) {
   }
 }
 
+Code new_code(byte *bytecode, int num_opcodes) {
+  Code result;
+  result.bytecode = bytecode;
+  result.num_opcodes = num_opcodes;
+  result.caches = calloc(num_opcodes, sizeof *result.caches);
+  return result;
+}
+
 int main() {
   byte bytecode[] = {/*0:*/ ARG,   0,
                      /*2:*/ ARG,   1,
                      /*4:*/ ADD,   0,
                      /*6:*/ PRINT, 0,
                      /*8:*/ HALT,  0};
-  Object consts[] = {
-      new_int(3),
-      new_int(4),
-      new_str("szechuan "),
-      new_str("broccoli"),
-  };
   Object int_args[] = {
       new_int(5),
       new_int(10),
@@ -225,10 +246,10 @@ int main() {
       new_str("hello "),
       new_str("world"),
   };
-  Code code = new_code(bytecode, sizeof bytecode / kBytecodeSize, consts,
-                       ARRAYSIZE(consts));
-  eval_code(&code, int_args, ARRAYSIZE(int_args));
-  eval_code(&code, int_args, ARRAYSIZE(int_args));
-  eval_code(&code, str_args, ARRAYSIZE(str_args));
-  eval_code(&code, str_args, ARRAYSIZE(str_args));
+  Code code = new_code(bytecode, sizeof bytecode / kBytecodeSize);
+  void (*eval)() = eval_code_cached;
+  eval(&code, int_args, ARRAYSIZE(int_args));
+  eval(&code, int_args, ARRAYSIZE(int_args));
+  eval(&code, str_args, ARRAYSIZE(str_args));
+  eval(&code, str_args, ARRAYSIZE(str_args));
 }
