3841d362aea4fc10e6c63ca9a7c8cd12c95bcff0
Max Bernstein <emacs@fb.com>
2021-01-27 20:33:41 -0800

    Add State struct; add quickening
    Also add command line options to switch between

--- interpreter.c
+++ interpreter.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define FORCE_INLINE __attribute__((always_inline)) inline
+
 // TODO(max): Consider writing this in Rust or Nim for some extra reader
 // appeal.
 
@@ -120,6 +122,8 @@ typedef enum {
   ARG,
   // Add stack[-2] + stack[-1].
   ADD,
+  // Specialized ADD for integers.
+  ADD_INT,
   // Pop the top of the stack and print it.
   PRINT,
   // Halt the machine.
@@ -141,31 +145,57 @@ Method lookup_method(ObjectType type, Symbol name) {
 
 static unsigned kBytecodeSize = 2;
 
-void eval_code_uncached(Code *code, Object *args, int nargs) {
-  int pc = 0;
 #define STACK_SIZE 100
+
+typedef struct {
   Object stack_array[STACK_SIZE];
-  Object *stack = stack_array;
-#define PUSH(x) *stack++ = (x)
-#define POP() *--stack
+  Object *stack;
+  Code *code;
+  int pc;
+} State;
+
+static FORCE_INLINE void push(State *state, Object value) {
+  CHECK(state->stack - state->stack_array < STACK_SIZE && "stack overflow");
+  *state->stack++ = value;
+}
+
+static FORCE_INLINE Object pop(State *state) {
+  CHECK(state->stack > state->stack_array && "stack underflow");
+  return *--state->stack;
+}
+
+static FORCE_INLINE Object peek(State *state, int pos) {
+  CHECK(state->stack - pos >= state->stack_array && "stack underflow");
+  return state->stack[-pos];
+}
+
+void init_state(State *state, Code *code) {
+  state->pc = 0;
+  state->stack = state->stack_array;
+  state->code = code;
+}
+
+void eval_code_uncached(Code *code, Object *args, int nargs) {
+  State state;
+  init_state(&state, code);
   while (true) {
-    Opcode op = code->bytecode[pc];
-    byte arg = code->bytecode[pc + 1];
+    Opcode op = code->bytecode[state.pc];
+    byte arg = code->bytecode[state.pc + 1];
     switch (op) {
       case ARG:
         CHECK(arg < nargs && "out of bounds arg");
-        PUSH(args[arg]);
+        push(&state, args[arg]);
         break;
       case ADD: {
-        Object right = POP();
-        Object left = POP();
+        Object right = pop(&state);
+        Object left = pop(&state);
         Method method = lookup_method(left.type, kAdd);
         Object result = (*method)(left, right);
-        PUSH(result);
+        push(&state, result);
         break;
       }
       case PRINT: {
-        Object obj = POP();
+        Object obj = pop(&state);
         Method method = lookup_method(obj.type, kPrint);
         (*method)(obj);
         break;
@@ -176,44 +206,59 @@ void eval_code_uncached(Code *code, Object *args, int nargs) {
         fprintf(stderr, "unknown opcode %d\n", op);
         abort();
     }
-    pc += kBytecodeSize;
+    state.pc += kBytecodeSize;
   }
 }
 
+static FORCE_INLINE CachedValue cache_at(State *state) {
+  return state->code->caches[state->pc / kBytecodeSize];
+}
+
+static FORCE_INLINE void cache_at_put(State *state, CachedValue value) {
+  state->code->caches[state->pc / kBytecodeSize] = value;
+}
+
+void do_add_cached(State *state) {
+  Object right = pop(state);
+  Object left = pop(state);
+  CachedValue cached = cache_at(state);
+  Method method = cached.value;
+  if (method == NULL || cached.key != left.type) {
+    fprintf(stderr, "updating cache at %d\n", state->pc);
+    method = lookup_method(left.type, kAdd);
+    cache_at_put(state, (CachedValue){.key = left.type, .value = method});
+  } else {
+    fprintf(stderr, "using cached value at %d\n", state->pc);
+  }
+  Object result = (*method)(left, right);
+  push(state, result);
+}
+
+void do_add_int(State *state) {
+  Object right = pop(state);
+  Object left = pop(state);
+  // Assume int
+  Object result = int_add(left, right);
+  push(state, result);
+}
+
 void eval_code_cached(Code *code, Object *args, int nargs) {
-  int pc = 0;
-#define STACK_SIZE 100
-  Object stack_array[STACK_SIZE];
-  Object *stack = stack_array;
-#define PUSH(x) *stack++ = (x)
-#define POP() *--stack
-#define CACHE_AT(pc) code->caches[(pc) / kBytecodeSize]
+  State state;
+  init_state(&state, code);
   while (true) {
-    Opcode op = code->bytecode[pc];
-    byte arg = code->bytecode[pc + 1];
+    Opcode op = code->bytecode[state.pc];
+    byte arg = code->bytecode[state.pc + 1];
     switch (op) {
       case ARG:
         CHECK(arg < nargs && "out of bounds arg");
-        PUSH(args[arg]);
+        push(&state, args[arg]);
         break;
       case ADD: {
-        Object right = POP();
-        Object left = POP();
-        CachedValue cached = CACHE_AT(pc);
-        Method method = cached.value;
-        if (method == NULL || cached.key != left.type) {
-          fprintf(stderr, "updating cache at %d\n", pc);
-          method = lookup_method(left.type, kAdd);
-          CACHE_AT(pc) = (CachedValue){.key = left.type, .value = method};
-        } else {
-          fprintf(stderr, "using cached value at %d\n", pc);
-        }
-        Object result = (*method)(left, right);
-        PUSH(result);
+        do_add_cached(&state);
         break;
       }
       case PRINT: {
-        Object obj = POP();
+        Object obj = pop(&state);
         Method method = lookup_method(obj.type, kPrint);
         (*method)(obj);
         break;
@@ -224,7 +269,55 @@ void eval_code_cached(Code *code, Object *args, int nargs) {
         fprintf(stderr, "unknown opcode %d\n", op);
         abort();
     }
-    pc += kBytecodeSize;
+    state.pc += kBytecodeSize;
+  }
+}
+
+void eval_code_quickening(Code *code, Object *args, int nargs) {
+  State state;
+  init_state(&state, code);
+  while (true) {
+    Opcode op = code->bytecode[state.pc];
+    byte arg = code->bytecode[state.pc + 1];
+    switch (op) {
+      case ARG:
+        CHECK(arg < nargs && "out of bounds arg");
+        push(&state, args[arg]);
+        break;
+      case ADD: {
+        if (peek(&state, 2).type == kInt) {
+          // Rewrite to specialized handler
+          fprintf(stderr, "rewriting ADD to ADD_INT at %d\n", state.pc);
+          code->bytecode[state.pc] = ADD_INT;
+          do_add_int(&state);
+          break;
+        }
+        do_add_cached(&state);
+        break;
+      }
+      case ADD_INT: {
+        if (peek(&state, 2).type != kInt) {
+          // Rewrite to generic handler
+          code->bytecode[state.pc] = ADD;
+          do_add_cached(&state);
+          break;
+        }
+        do_add_int(&state);
+        break;
+      }
+      case PRINT: {
+        Object obj = pop(&state);
+        Method method = lookup_method(obj.type, kPrint);
+        (*method)(obj);
+        break;
+      }
+      case HALT:
+        return;
+      default:
+        fprintf(stderr, "unknown opcode %d\n", op);
+        abort();
+    }
+    state.pc += kBytecodeSize;
   }
 }
 
@@ -236,7 +329,24 @@ Code new_code(byte *bytecode, int num_opcodes) {
   return result;
 }
 
-int main() {
+int main(int argc, char **argv) {
+  void (*eval)() = eval_code_uncached;
+  if (argc == 2) {
+    const char *mode = argv[1];
+    if (strcmp(mode, "uncached") == 0) {
+      eval = eval_code_uncached;
+    } else if (strcmp(mode, "cached") == 0) {
+      eval = eval_code_cached;
+    } else if (strcmp(mode, "quickening") == 0) {
+      eval = eval_code_quickening;
+    } else {
+      fprintf(stderr, "Usage: ./interpreter [uncached|cached|quickening]\n");
+      return EXIT_FAILURE;
+    }
+  } else if (argc > 2) {
+    fprintf(stderr, "Usage: ./interpreter [uncached|cached|quickening]\n");
+    return EXIT_FAILURE;
+  }
   byte bytecode[] = {/*0:*/ ARG,   0,
                      /*2:*/ ARG,   1,
                      /*4:*/ ADD,   0,
@@ -251,7 +361,6 @@ int main() {
       new_str("world"),
   };
   Code code = new_code(bytecode, sizeof bytecode / kBytecodeSize);
-  void (*eval)() = eval_code_cached;
   eval(&code, int_args, ARRAYSIZE(int_args));
   eval(&code, int_args, ARRAYSIZE(int_args));
   eval(&code, str_args, ARRAYSIZE(str_args));
