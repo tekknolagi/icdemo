8713288e43fecd598c0428cf48326d4b5423ec99
Max Bernstein <emacs@fb.com>
2021-02-04 23:19:40 -0800

    Replace Object struct with Object pointers
    Opaque pointers makes it easier to optimize things in later chapters

--- interpreter.c
+++ interpreter.c
@@ -5,30 +5,6 @@
 
 #define FORCE_INLINE __attribute__((always_inline)) inline
 
-// TODO(max): Consider writing this in Rust or Nim for some extra reader
-// appeal.
-
-typedef enum {
-  kInt,
-  kStr,
-} ObjectType;
-
-typedef struct {
-  ObjectType type;
-  union {
-    const char *str_value;
-    int int_value;
-  };
-} Object;
-
-Object new_int(int value) {
-  return (Object){.type = kInt, .int_value = value};
-}
-
-Object new_str(const char *value) {
-  return (Object){.type = kStr, .str_value = value};
-}
-
 #if defined(NDEBUG)
 #define CHECK
 #else
@@ -40,31 +16,78 @@ Object new_str(const char *value) {
   } while (0)
 #endif
 
-Object int_add(Object left, Object right) {
-  CHECK(left.type == kInt);
-  CHECK(right.type == kInt);
-  return new_int(left.int_value + right.int_value);
+// TODO(max): Consider writing this in Rust or Nim for some extra reader
+// appeal.
+
+typedef enum {
+  kInt,
+  kStr,
+} ObjectType;
+
+typedef struct {
+  ObjectType type;
+  union {
+    const char* str_value;
+    int int_value;
+  };
+} Object;
+
+ObjectType object_type(Object* obj) { return obj->type; }
+
+bool object_is_int(Object* obj) { return obj->type == kInt; }
+
+bool object_is_str(Object* obj) { return obj->type == kStr; }
+
+int object_as_int(Object* obj) {
+  CHECK(object_is_int(obj));
+  return obj->int_value;
 }
 
-Object int_print(Object obj) {
-  CHECK(obj.type == kInt);
-  fprintf(stderr, "int: %d\n", obj.int_value);
+const char* object_as_str(Object* obj) {
+  CHECK(object_is_str(obj));
+  return obj->str_value;
+}
+
+Object* new_int(int value) {
+  Object* result = malloc(sizeof *result);
+  CHECK(result != NULL && "could not allocate object");
+  *result = (Object){.type = kInt, .int_value = value};
+  return result;
+}
+
+Object* new_str(const char* value) {
+  Object* result = malloc(sizeof *result);
+  CHECK(result != NULL && "could not allocate object");
+  *result = (Object){.type = kStr, .str_value = value};
+  return result;
+}
+
+Object* int_add(Object* left, Object* right) {
+  CHECK(object_is_int(left));
+  CHECK(object_is_int(right));
+  return new_int(object_as_int(left) + object_as_int(right));
+}
+
+Object* int_print(Object* obj) {
+  CHECK(object_is_int(obj));
+  fprintf(stderr, "int: %d\n", object_as_int(obj));
   return obj;
 }
 
-Object str_add(Object left, Object right) {
-  CHECK(left.type == kStr);
-  CHECK(right.type == kStr);
-  int result_size = strlen(left.str_value) + strlen(right.str_value) + 1;
-  char *result = malloc(result_size);
-  strcpy(result, left.str_value);
-  strcat(result, right.str_value);
+Object* str_add(Object* left, Object* right) {
+  CHECK(object_is_str(left));
+  CHECK(object_is_str(right));
+  int result_size =
+      strlen(object_as_str(left)) + strlen(object_as_str(right)) + 1;
+  char* result = malloc(result_size);
+  strcpy(result, object_as_str(left));
+  strcat(result, object_as_str(right));
   return new_str(result);
 }
 
-Object str_print(Object obj) {
-  CHECK(obj.type == kStr);
-  fprintf(stderr, "str: \"%s\"\n", obj.str_value);
+Object* str_print(Object* obj) {
+  CHECK(object_is_str(obj));
+  fprintf(stderr, "str: \"%s\"\n", object_as_str(obj));
   return obj;
 }
 
@@ -78,7 +101,7 @@ typedef enum {
 // Note: this takes advantage of the fact that in C, not putting anything
 // between the parentheses means that this function can take any number of
 // arguments.
-typedef Object (*Method)();
+typedef Object* (*Method)();
 
 typedef struct {
   Symbol name;
@@ -101,7 +124,7 @@ static const MethodDefinition kStrMethods[] = {
 // method tables will be sparsely populated (not every type will implement
 // every method) but the type table should contain every type. This is not an
 // essential design decision.
-static const MethodDefinition *kTypes[] = {
+static const MethodDefinition* kTypes[] = {
     [kInt] = kIntMethods,
     [kStr] = kStrMethods,
 };
@@ -115,10 +138,10 @@ typedef struct {
 
 typedef struct {
   // Array of `num_opcodes' (op, arg) pairs (total size `num_opcodes' * 2).
-  byte *bytecode;
+  byte* bytecode;
   int num_opcodes;
   // Array of `num_opcodes' elements.
-  CachedValue *caches;
+  CachedValue* caches;
 } Code;
 
 typedef enum {
@@ -140,7 +163,7 @@ typedef enum {
 
 Method lookup_method(ObjectType type, Symbol name) {
   CHECK(type < ARRAYSIZE(kTypes) && "out of bounds type");
-  const MethodDefinition *table = kTypes[type];
+  const MethodDefinition* table = kTypes[type];
   for (int i = 0; table[i].method != NULL; i++) {
     if (table[i].name == name) {
       return table[i].method;
@@ -154,29 +177,29 @@ static unsigned kBytecodeSize = 2;
 #define STACK_SIZE 100
 
 typedef struct {
-  Object stack_array[STACK_SIZE];
-  Object *stack;
-  Code *code;
+  Object* stack_array[STACK_SIZE];
+  Object** stack;
+  Code* code;
   int pc;
 } Frame;
 
-static FORCE_INLINE void push(Frame *frame, Object value) {
+static FORCE_INLINE void push(Frame* frame, Object* value) {
   CHECK(frame->stack - frame->stack_array < STACK_SIZE && "stack overflow");
   *frame->stack++ = value;
 }
 
-static FORCE_INLINE Object pop(Frame *frame) {
+static FORCE_INLINE Object* pop(Frame* frame) {
   CHECK(frame->stack > frame->stack_array && "stack underflow");
   return *--frame->stack;
 }
 
-void init_frame(Frame *frame, Code *code) {
+void init_frame(Frame* frame, Code* code) {
   frame->pc = 0;
   frame->stack = frame->stack_array;
   frame->code = code;
 }
 
-void eval_code_uncached(Code *code, Object *args, int nargs) {
+void eval_code_uncached(Code* code, Object** args, int nargs) {
   Frame frame;
   init_frame(&frame, code);
   while (true) {
@@ -188,16 +211,16 @@ void eval_code_uncached(Code *code, Object *args, int nargs) {
         push(&frame, args[arg]);
         break;
       case ADD: {
-        Object right = pop(&frame);
-        Object left = pop(&frame);
-        Method method = lookup_method(left.type, kAdd);
-        Object result = (*method)(left, right);
+        Object* right = pop(&frame);
+        Object* left = pop(&frame);
+        Method method = lookup_method(object_type(left), kAdd);
+        Object* result = (*method)(left, right);
         push(&frame, result);
         break;
       }
       case PRINT: {
-        Object obj = pop(&frame);
-        Method method = lookup_method(obj.type, kPrint);
+        Object* obj = pop(&frame);
+        Method method = lookup_method(object_type(obj), kPrint);
         (*method)(obj);
         break;
       }
@@ -211,25 +234,25 @@ void eval_code_uncached(Code *code, Object *args, int nargs) {
   }
 }
 
-static FORCE_INLINE CachedValue cache_at(Frame *frame) {
+static FORCE_INLINE CachedValue cache_at(Frame* frame) {
   return frame->code->caches[frame->pc / kBytecodeSize];
 }
 
-static FORCE_INLINE void cache_at_put(Frame *frame, ObjectType key,
+static FORCE_INLINE void cache_at_put(Frame* frame, ObjectType key,
                                       Method value) {
   frame->code->caches[frame->pc / kBytecodeSize] =
       (CachedValue){.key = key, .value = value};
 }
 
-void add_update_cache(Frame *frame, Object left, Object right) {
-  Method method = lookup_method(left.type, kAdd);
+void add_update_cache(Frame* frame, Object* left, Object* right) {
+  Method method = lookup_method(object_type(left), kAdd);
   fprintf(stderr, "updating cache at %d\n", frame->pc);
-  cache_at_put(frame, left.type, method);
-  Object result = (*method)(left, right);
+  cache_at_put(frame, object_type(left), method);
+  Object* result = (*method)(left, right);
   push(frame, result);
 }
 
-void eval_code_cached(Code *code, Object *args, int nargs) {
+void eval_code_cached(Code* code, Object** args, int nargs) {
   Frame frame;
   init_frame(&frame, code);
   while (true) {
@@ -241,22 +264,22 @@ void eval_code_cached(Code *code, Object *args, int nargs) {
         push(&frame, args[arg]);
         break;
       case ADD: {
-        Object right = pop(&frame);
-        Object left = pop(&frame);
+        Object* right = pop(&frame);
+        Object* left = pop(&frame);
         CachedValue cached = cache_at(&frame);
         Method method = cached.value;
-        if (method == NULL || cached.key != left.type) {
+        if (method == NULL || cached.key != object_type(left)) {
           add_update_cache(&frame, left, right);
           break;
         }
         fprintf(stderr, "using cached value at %d\n", frame.pc);
-        Object result = (*method)(left, right);
+        Object* result = (*method)(left, right);
         push(&frame, result);
         break;
       }
       case PRINT: {
-        Object obj = pop(&frame);
-        Method method = lookup_method(obj.type, kPrint);
+        Object* obj = pop(&frame);
+        Method method = lookup_method(object_type(obj), kPrint);
         (*method)(obj);
         break;
       }
@@ -270,12 +293,12 @@ void eval_code_cached(Code *code, Object *args, int nargs) {
   }
 }
 
-void do_add_int(Frame *frame, Object left, Object right) {
-  Object result = int_add(left, right);
+void do_add_int(Frame* frame, Object* left, Object* right) {
+  Object* result = int_add(left, right);
   push(frame, result);
 }
 
-void eval_code_quickening(Code *code, Object *args, int nargs) {
+void eval_code_quickening(Code* code, Object** args, int nargs) {
   Frame frame;
   init_frame(&frame, code);
   while (true) {
@@ -287,9 +310,9 @@ void eval_code_quickening(Code *code, Object *args, int nargs) {
         push(&frame, args[arg]);
         break;
       case ADD: {
-        Object right = pop(&frame);
-        Object left = pop(&frame);
-        if (left.type == kInt) {
+        Object* right = pop(&frame);
+        Object* left = pop(&frame);
+        if (object_type(left) == kInt) {
           do_add_int(&frame, left, right);
           code->bytecode[frame.pc] = ADD_INT;
           break;
@@ -299,23 +322,23 @@ void eval_code_quickening(Code *code, Object *args, int nargs) {
         break;
       }
       case ADD_CACHED: {
-        Object right = pop(&frame);
-        Object left = pop(&frame);
+        Object* right = pop(&frame);
+        Object* left = pop(&frame);
         CachedValue cached = cache_at(&frame);
-        if (cached.key != left.type) {
+        if (cached.key != object_type(left)) {
           add_update_cache(&frame, left, right);
           break;
         }
         fprintf(stderr, "using cached value at %d\n", frame.pc);
         Method method = cached.value;
-        Object result = (*method)(left, right);
+        Object* result = (*method)(left, right);
         push(&frame, result);
         break;
       }
       case ADD_INT: {
-        Object right = pop(&frame);
-        Object left = pop(&frame);
-        if (left.type != kInt) {
+        Object* right = pop(&frame);
+        Object* left = pop(&frame);
+        if (object_type(left) != kInt) {
           add_update_cache(&frame, left, right);
           code->bytecode[frame.pc] = ADD_CACHED;
           break;
@@ -324,8 +347,8 @@ void eval_code_quickening(Code *code, Object *args, int nargs) {
         break;
       }
       case PRINT: {
-        Object obj = pop(&frame);
-        Method method = lookup_method(obj.type, kPrint);
+        Object* obj = pop(&frame);
+        Method method = lookup_method(object_type(obj), kPrint);
         (*method)(obj);
         break;
       }
@@ -339,7 +362,7 @@ void eval_code_quickening(Code *code, Object *args, int nargs) {
   }
 }
 
-Code new_code(byte *bytecode, int num_opcodes) {
+Code new_code(byte* bytecode, int num_opcodes) {
   Code result;
   result.bytecode = bytecode;
   result.num_opcodes = num_opcodes;
@@ -347,10 +370,10 @@ Code new_code(byte *bytecode, int num_opcodes) {
   return result;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   void (*eval)() = eval_code_uncached;
   if (argc == 2) {
-    const char *mode = argv[1];
+    const char* mode = argv[1];
     if (strcmp(mode, "uncached") == 0) {
       eval = eval_code_uncached;
     } else if (strcmp(mode, "cached") == 0) {
@@ -370,11 +393,11 @@ int main(int argc, char **argv) {
                      /*4:*/ ADD,   0,
                      /*6:*/ PRINT, 0,
                      /*8:*/ HALT,  0};
-  Object int_args[] = {
+  Object* int_args[] = {
       new_int(5),
       new_int(10),
   };
-  Object str_args[] = {
+  Object* str_args[] = {
       new_str("hello "),
       new_str("world"),
   };
